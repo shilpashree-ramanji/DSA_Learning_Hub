b1d8c5a73f721377e3bf3c64e79ac5204e0469fb
shilpashree-ramanji <shilpa.ramanji.99@gmail.com>
2026-04-23 23:11:59 +0530

    test commit

--- Core_Data_Strutures/example.py
+++ Core_Data_Strutures/example.py
@@ -1,9 +1,1 @@
-def findSum(n):
-    sum = 0
-    i = 1
-    
-    # Iterating over all the numbers between 1 to n
-    while i <= n:
-        sum = sum + i
-        i = i + 1
-    return sum
+## empty files
