e9db86afdf27e0b2a55f6ad1e1d147576fe756ba
shilpashree-ramanji <shilpa.ramanji.99@gmail.com>
2026-04-25 22:52:10 +0530

    updating a exaple witl DocString

--- Core_Data_Strutures/Recursion.py
+++ Core_Data_Strutures/Recursion.py
@@ -1,3 +1,22 @@
+'''
+The process in which a function calls itself directly or indirectly is called recursion and the corresponding function is called a recursive function.
+
+A recursive algorithm takes one step toward solution and then recursively call itself to further move. The algorithm stops once we reach the solution.
+Since called function may further call itself, this process might continue forever. So it is essential to provide a base case to terminate this recursion process.
+Steps to Implement Recursion
+
+Step1 - Define a base case: Identify the simplest (or base) case for which the solution is known or trivial. This is the stopping condition for the recursion, as it prevents the function from infinitely calling itself.
+
+Step2 - Define a recursive case: Define the problem in terms of smaller subproblems. Break the problem down into smaller versions of itself, and call the function recursively to solve each subproblem.
+
+Step3 - Ensure the recursion terminates: Make sure that the recursive function eventually reaches the base case, and does not enter an infinite loop.
+
+Step4 - Combine the solutions: Combine the solutions of the subproblems to solve the original problem.
+
+
+
+'''
+
 def sum(n):
     
     # base condition
